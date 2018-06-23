5f9f250321c45360ce7937d79d58c583f8eb6bb5
matt <mknapper1@gmail.com>
2018-06-23 11:53:59 -0400

    Created models

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'shiftbot.users.apps.UsersAppConfig',
+    'schedule.apps.ScheduleConfig'
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
--- schedule/models.py
+++ schedule/models.py
@@ -1,20 +1,59 @@
 from django.db import models
 
-# send help
+
 class Workplace(models.Model):
-    Name = models.CharField(max_length=255)
-    BossName = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    boss_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
 
 class Shift(models.Model):
-    StartTime = models.DateTimeField()
-    EndTime = models.DateTimeField()
+    name = models.CharField(max_length=255, blank=True, null=True)
+    sunday = 'Sunday'
+    monday = 'Monday'
+    tuesday = 'Tuesday'
+    wednesday = 'Wednesday'
+    thursday = 'Thursday'
+    friday = 'Friday'
+    saturday = 'Saturday'
+    DAY_CHOICES = (
+        (sunday, 'Sunday'),
+        (monday, 'Monday'),
+        (tuesday, 'Tuesday'),
+        (wednesday, 'Wednesday'),
+        (thursday, 'Thursday'),
+        (friday, 'Friday'),
+        (saturday, 'Saturday'),
+    )
+    day = models.CharField(max_length=10, choices=DAY_CHOICES, default=sunday)
+    week = models.IntegerField(default=1)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    job = models.ForeignKey(Job, null=True, blank=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return self.name
 
 
 class Employee(models.Model):
-    Name = models.CharField(max_length=255)
-    PhoneNumber = models.CharField(max_length=12)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=12)
+    job = models.ForeignKey(Job, null=True, blank=True, on_delete=models.SET_NULL)
+    workplace = models.ForeignKey(Workplace, null=True, blank=True, on_delete=models.SET_NULL)
 
-class Job(models.Model):
-    Name = models.CharField(max_length=255)
-    Description = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+
+
+
 
