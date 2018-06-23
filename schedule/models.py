793de53db2d85d11c51d1d30ae4394017ba3438b
matt <mknapper1@gmail.com>
2018-06-23 10:18:03 -0400

    added basic models

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# send help
+class Workplace(models.Model):
+    Name = models.CharField(max_length=255)
+    BossName = models.CharField(max_length=255)
+
+class Shift(models.Model):
+    StartTime = models.DateTimeField()
+    EndTime = models.DateTimeField()
+
+
+class Employee(models.Model):
+    Name = models.CharField(max_length=255)
+    PhoneNumber = models.CharField(max_length=12)
+
+class Job(models.Model):
+    Name = models.CharField(max_length=255)
+    Description = models.CharField(max_length=255)
+
