23a698e3f9909dede221b7b5a8ca61ad6d78b178
mknapper1 <mknapper1@gmail.com>
2018-06-24 04:28:41 -0400

    fixed some login stuff

--- schedule/views.py
+++ schedule/views.py
@@ -1,5 +1,6 @@
-import datetime
+from datetime import datetime
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -7,15 +8,17 @@ from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
 from .forms import EmployeeForm, JobForm, ShiftForm, AjaxShiftForm
-from .models import WorkWeek
+from .models import WorkWeek, Shift, Job
 
 
+@login_required
 def dashboard(request):
-    week = datetime.datetime.now().isocalendar()[1]
-    year = datetime.datetime.now().year
+    week = datetime.now().isocalendar()[1]
+    year = datetime.now().year
     return render(request, 'schedule/dashboard.html', {'week': week, 'year': year})
 
 
+@login_required
 def schedule_create_view(request, year=None, week=None):
     work_week, created = WorkWeek.objects.get_or_create(year=year, week=week, workplace=request.user.workplace)
     shifts = work_week.shifts.all() if not created else None
@@ -31,16 +34,41 @@ def ajax_create_shift(request):
     if request.method == 'POST':
         form = AjaxShiftForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_date
-            return JsonResponse({'shift_id': '123'})
+            cd = form.cleaned_data
+            weekday = cd['work_day']
+            workplace = request.user.workplace
+            work_week = WorkWeek.objects.get(pk=cd['work_week'])
+            job = Job.objects.get(pk=cd['job'])
+            start_time = cd['start_time']
+            end_time = cd['end_time']
+            try:
+                new_shift = Shift.objects.create(weekday=weekday,
+                                                 workplace=workplace,
+                                                 work_week=work_week,
+                                                 job=job,
+                                                 start_time=start_time,
+                                                 end_time=end_time)
+                new_shift.save()
+            except Exception as E:
+                print(E)
+
+            return JsonResponse({'shift_id': new_shift.id,
+                                 'job': new_shift.job.name,
+                                 'job_slug': new_shift.job.slug,
+                                 'start_time': new_shift.start_datetime().isoformat(),
+                                 'end_time': new_shift.end_datetime().isoformat(),
+                                 'background_color': new_shift.job.color
+                                 })
     return JsonResponse({'error': 'sorry'})
 
 
+@login_required
 def employees_view(request):
     employees = request.user.workplace.employee_set.all()
     return render(request, 'schedule/employee/list.html', {'employees':employees})
 
 
+@login_required
 def employees_new_view(request):
     if request.method == 'POST':
         form = EmployeeForm(request.POST)
@@ -54,11 +82,13 @@ def employees_new_view(request):
     return render(request, 'schedule/employee/create.html', {'form': form})
 
 
+@login_required
 def jobs_view(request):
     jobs = request.user.workplace.job_set.all()
     return render(request, 'schedule/job/list.html', {'jobs': jobs})
 
 
+@login_required
 def jobs_new_view(request):
     if request.method == 'POST':
         form = JobForm(request.POST)
@@ -73,11 +103,13 @@ def jobs_new_view(request):
     return render(request, 'schedule/job/create.html', {'form': form})
 
 
+@login_required
 def shifts_view(request):
     shifts = request.user.workplace.shift_set.all()
     return render(request, 'schedule/shift/list.html', {'shifts': shifts})
 
 
+@login_required
 def shifts_new_view(request):
     if request.method == 'POST':
         form = ShiftForm(request.POST)
@@ -91,5 +123,7 @@ def shifts_new_view(request):
 
     return render(request, 'schedule/shift/create.html', {'form': form})
 
+
+@login_required
 def dummy_view(request):
     return render(request, 'schedule/schedule/create.html', {})
